9527a8db11d0f03cb9222a2ed42682a6eeed3110
MahdiHabibiXr <themahdihabibi@gmail.com>
2024-08-27 01:58:11 +0330

    tasks

--- main.py
+++ main.py
@@ -25,34 +25,36 @@ async def start(client, message):
 
 @bot.on_message(filters.private & filters.photo)
 async def image(client, message):
-    chat_id = message.chat.id
-
-    user_root = f'{root}{chat_id}/'
-    file_name = ''
-    
-    
-    if(os.path.exists(f'{user_root}person.jpg') and os.path.exists(f'{user_root}garment.jpg')):
-        #both photos are sent
-        await message.reply('You have submitted person and garment images before\nto submit new images, Use the commans: /edit')
-    else:
-        if(not os.path.isdir(user_root)):
-            #first time sending photo so we create the user dir
-            os.makedirs(user_root)
-            file_name = 'person.jpg'
-            await message.reply('Person image submitted, Now send me a upper body garment image.')
-
-        elif(not os.path.exists(f'{user_root}person.jpg') and not os.path.exists(f'{user_root}garment.jpg')):
-            #sent person photo
-            file_name = 'person.jpg'
-            await message.reply('Person image submitted, Now send me a upper body garment image.')
-
-        elif(os.path.exists(f'{user_root}person.jpg') and not os.path.exists(f'{user_root}garment.jpg')):
-            #sent garment
-            file_name = 'garment.jpg'
-            await message.reply('Ok now use the command /imagine_upper (for upperbody garment) or /imagine_lower (for lowerbody garment) or /imagine_dress (for dress garment) and describe the garment in order to generate the photo, example \n /imagine_upper black t shirt')
+    if(message.from_user.is_bot == False) : 
         
+        chat_id = message.chat.id
+
+        user_root = f'{root}{chat_id}/'
+        file_name = ''
+        
+        
+        if(os.path.exists(f'{user_root}person.jpg') and os.path.exists(f'{user_root}garment.jpg')):
+            #both photos are sent
+            await message.reply('You have submitted person and garment images before\nto submit new images, Use the commans: /edit')
+        else:
+            if(not os.path.isdir(user_root)):
+                #first time sending photo so we create the user dir
+                os.makedirs(user_root)
+                file_name = 'person.jpg'
+                await message.reply('Person image submitted, Now send me a upper body garment image.')
+
+            elif(not os.path.exists(f'{user_root}person.jpg') and not os.path.exists(f'{user_root}garment.jpg')):
+                #sent person photo
+                file_name = 'person.jpg'
+                await message.reply('Person image submitted, Now send me a upper body garment image.')
+
+            elif(os.path.exists(f'{user_root}person.jpg') and not os.path.exists(f'{user_root}garment.jpg')):
+                #sent garment
+                file_name = 'garment.jpg'
+                await message.reply('Ok now use the command /imagine_upper (for upperbody garment) or /imagine_lower (for lowerbody garment) or /imagine_dress (for dress garment) and describe the garment in order to generate the photo, example \n /imagine_upper black t shirt')
             
-        file = await client.download_media(message.photo.file_id, file_name = f'{user_root}{file_name}')
+                
+            file = await client.download_media(message.photo.file_id, file_name = f'{user_root}{file_name}')
 
 @bot.on_message(filters.private & filters.regex('/edit'))
 async def edit_image(client, message):
@@ -142,23 +144,7 @@ async def images(client, message):
         await message.reply('No images has been submitted, Send person image.')
 
 
-@bot.on_message(filters.private & filters.regex('/save'))
-async def save(client, message):
-    chat_id = message.chat.id
-    url = message.text.split(' ')[1]
 
-    downloaded_path = download_image(url, chat_id)
-    await client.send_photo(chat_id, downloaded_path, caption='your image')
-
-@bot.on_message(filters.private & filters.regex('/resize'))
-async def resize(client, message):
-    chat_id = message.chat.id
-    person_img = f'{root}{chat_id}/person.jpg'
-    garment_img = f'{root}{chat_id}/garment.jpg'
-    target = 'outputs/791927771-1.jpg'
-
-    resized = resize_image(person_img, target)
-    await client.send_photo(chat_id, resized, caption='resized image')
 
 @bot.on_message(filters.private & filters.command('run_auto_on'))
 def run_auto_on(client, message):
@@ -170,34 +156,5 @@ def run_auto_on(client, message):
     os.environ['BACKEND_ON'] = 'False'
     message.reply('Tasks not doing enymore, to turn on /run_auto_on')
 
-def download_image(url, id):
-    response = requests.get(url)
-    image = Image.open(BytesIO(response.content))
 
-    count = len(os.listdir('outputs/')) + 1
-    filename = f"outputs/{id}-{count}.jpg"
-
-    image.save(filename)
-    print(f'Image successfully downloaded and saved as {filename}')
-
-    return filename
-
-def resize_image(target, source):
-    # Open the target image and the source image
-    target_image = Image.open(target)
-    source_image = Image.open(source)
-
-    # Get the size of the target image
-    target_size = target_image.size
-
-    # Resize the source image to match the size of the target image
-    resized_source_image = source_image.resize(target_size)
-
-    # Save the resized image (optional)
-    resized_source_image.save(source)
-
-    # Display the resized image (optional)
-    resized_source_image.show()
-
-    return(source)
 bot.run()
